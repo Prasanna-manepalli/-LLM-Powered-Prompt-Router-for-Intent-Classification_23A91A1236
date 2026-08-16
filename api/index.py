422043817f11ecdda71d5a5d9b8479c75680e369
Prasanna986642 <manepalli408@gmail.com>
2026-08-16 19:10:23 +0530

    initial commit

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import os
+import sys
+
+# Ensure root directory is in sys.path for Vercel Serverless
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.web import app
